bc0437e8e9791d5a6293a059a9a7e90ccc4015d8
ExtraHash <ExtraHash@protonmail.com>
2019-10-13 14:06:21 -0600

    add user-configurable coloring, substring removal, and line ignoring

--- src/Components/Terminal.tsx
+++ src/Components/Terminal.tsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
 import config from '../Constants/config.json';
+import substringsToRemove from '../Constants/substringsToRemove.json';
+import userColors from '../Constants/colorConfig.json';
+import linesToIgnore from '../Constants/linesToIgnore.json';
 
+const colorConfig: { [key: string]: any } = userColors;
+const colorValues = Object.keys(colorConfig);
 const { websocketHost, websocketPort } = config;
 
 const client = new W3CWebSocket(
@@ -43,13 +48,49 @@ class Terminal extends Component {
 
   render() {
     const { logHistory } = this.state;
+
     return (
       <div className="terminal">
         {logHistory.map((line, index) => {
+          // don't print empty messages
+          if (line.trim() === '') {
+            return null;
+          }
+
+          // ignore the line if it is in linesToIgnore.json
+          let showLine: boolean = true;
+          linesToIgnore.forEach((string: string) => {
+            if (line.includes(string)) {
+              console.log('nope nope nope!');
+              showLine = false;
+            }
+          });
+          if (!showLine) {
+            return null;
+          }
+
+          // let's determine the log color based on colorConfig.json
+          let logColor: string = '';
+          colorValues.forEach((color: string) => {
+            const { strings } = colorConfig[color];
+            strings.forEach((string: string) => {
+              if (line.includes(string)) {
+                logColor = colorConfig[color].class;
+              }
+            });
+          });
+
+          // Trim substrings that are in the substringsToRemove.json file
+          let logText: string = line;
+          substringsToRemove.forEach(string => {
+            logText = logText.replace(string, '');
+          });
+
+          // finally print the log message
           return (
-            <p key={index} className="logLine">
-              {line}
-            </p>
+            <span key={index} className={`is-family-monospace ${logColor}`}>
+              {logText}
+            </span>
           );
         })}
       </div>
